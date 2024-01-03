2206095d21faffe088793eee9de3419724993b38
valiana35 <144236658+valiana35@users.noreply.github.com>
2024-01-03 21:15:29 +0200

    hwjs7

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const controls = document.querySelector("#controls");
+const boxes = document.querySelector("#boxes");
+function createBoxes(amount) {
+  const arr = [];
+  for (let i = 1; i <= amount; i++) {
+    const str = `<div style="width: ${30}px;height=${30}px;"></div>`;
+    arr.push(str);
+  }
+}
