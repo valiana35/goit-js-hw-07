9c483dfc0c3af7ff41da5ed1317a9e3139febe4e
valiana35 <144236658+valiana35@users.noreply.github.com>
2024-01-03 15:47:50 +0200

    hwjs7

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const body = document.querySelector(".widget");
+const btn = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+btn.addEventListener("click", changeColor);
+function changeColor() {
+  const color = getRandomHexColor();
+  span.textContent = color;
+  body.style.backgroundColor = color;
+}
